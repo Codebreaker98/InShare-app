3e8ab55168517fc65bcf637d083a1983d7c4e727
Bhabani <bhabanivickyrath98@gmail.com>
2021-05-10 19:35:24 +0530

    Third commit

--- server.js
+++ server.js
@@ -5,20 +5,20 @@ const PORT = process.env.PORT || 3000;
 const path = require('path');
 const cors = require('cors');
 // Cors 
-//const corsOptions = {
-  //origin: process.env.ALLOWED_CLIENTS.split(',')
-  // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
-//}
+const corsOptions = {
+  origin: process.env.ALLOWED_CLIENTS.split(',')
+  //['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
+}
 
-// Default configuration looks like
-// {
+//Default configuration looks like
+//  {
 //     "origin": "*",
 //     "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
 //     "preflightContinue": false,
-//     "optionsSuccessStatus": 204
-//   }
+//      "optionsSuccessStatus": 204
+//    }
 
-//app.use(cors(corsOptions))
+app.use(cors(corsOptions))
 app.use(express.static('public'));
 
 const connectDB = require('./config/db');
